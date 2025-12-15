ee012130a3b4eb7e40b689508b800ca685db5283
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2025-12-15 23:34:45 +0530

    Freature: add CI pipwlinw

---
+++ test.js
@@ -0,0 +1,7 @@
+// test.js
+console.log("Running tests...");
+// In a real app, actual assertions would go here
+setTimeout(() => {
+    console.log("Tests in progress....!");
+}, 3000);
+console.log("Tests passed!");
